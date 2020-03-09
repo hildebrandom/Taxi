16dff1280dfa684315bc593ef0ffd22b4c234b9e
hildebrando monotya <hilderbrand2007@hotmail.com>
2020-03-08 20:28:03 -0500

    Add master detai

--- Taxi.Prism/Taxi.Prism/App.xaml.cs
+++ Taxi.Prism/Taxi.Prism/App.xaml.cs
@@ -26,6 +26,7 @@ namespace Taxi.Prism
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
+            containerRegistry.RegisterForNavigation<TaxiMasterDetailPage, TaxiMasterDetailPageViewModel>();
         }
     }
 }
